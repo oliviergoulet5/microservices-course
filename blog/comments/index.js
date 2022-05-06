900cd8e18b88f1c36fe64af680331e2625afc4fb
Olivier Goulet <olivier_goulet@icloud.com>
2022-05-06 19:51:00 -0400

    Add comments and post list

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,17 +1,19 @@
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const cors = require("cors");
 const express = require("express");
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 const commentsByPostId = {};
 
-app.get("posts/:id/comments", (req, res) => {
+app.get("/posts/:id/comments", (req, res) => {
     res.send(commentsByPostId[req.params.id || []]);
 });
 
-app.post("posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", (req, res) => {
     const commentId = randomBytes(4).toString("hex");
     const { content } = req.body;
 
