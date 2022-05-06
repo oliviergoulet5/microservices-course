721b54da826e1bbc3d0827bdb76b9d15e628fc92
Olivier Goulet <olivier_goulet@icloud.com>
2022-05-06 19:25:07 -0400

    Fix cors issue

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,8 +1,10 @@
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser")
+const cors = require("cors");
 const express = require("express");
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 const posts = {};
