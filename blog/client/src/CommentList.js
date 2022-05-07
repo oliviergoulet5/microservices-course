5672746313ad0b9b5e76583ba3fc6a85b3155b41
Olivier Goulet <olivier_goulet@icloud.com>
2022-05-06 22:53:12 -0400

    Add moderation to comments in web app

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function CommentList({ comments }) {
-    const renderedComments = comments.map(comment => <li key={comment.id}>{ comment.content }</li>);
+    const renderedComments = comments.map(comment => { 
+        let content;
+        if (comment.status === "approved") {
+            content = comment.content;
+        } else if (comment.status === "rejected") {
+            content = "This comment has been removed."
+        } else if (comment.status === "pending") {
+            content = "This comment is awaiting moderation."
+        }
+
+        return <li key={comment.id}>{ content }</li> 
+    });
 
     return (
         <ul>
